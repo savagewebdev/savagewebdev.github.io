d948fd8ad79566f03990cd9c8fb72c7d09ec3216
Robin Savage <robin.savage@protonmail.com>
2019-12-28 21:32:00 -0600

    Removed unused packages, mabox is now working

--- js/mapbox.js
+++ js/mapbox.js
@@ -1,5 +1,3 @@
-//const mapboxgl = require(['node_modules/mapbox-gl/dist/mapbox-gl.js']);
-
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iaW51c21heGltdXMiLCJhIjoiY2pzbm5qY2doMGU5dzQ0bjU2eWZiZWJkeCJ9.Cm0M0FhRGX6dnLhQB6wa7g';
 const map = new mapboxgl.Map({
 	container: 'map',
