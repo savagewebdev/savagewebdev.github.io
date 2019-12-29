1815c670387a0c49b669d6b72e1f996ae7e152c4
Robin Savage <robin.savage@protonmail.com>
2019-12-28 21:48:27 -0600

    Testing node.js linkage

--- js/mapbox.js
+++ js/mapbox.js
@@ -1,3 +1,5 @@
+var mapboxgl = require('mapbox-gl');
+
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iaW51c21heGltdXMiLCJhIjoiY2pzbm5qY2doMGU5dzQ0bjU2eWZiZWJkeCJ9.Cm0M0FhRGX6dnLhQB6wa7g';
 const map = new mapboxgl.Map({
 	container: 'map',
