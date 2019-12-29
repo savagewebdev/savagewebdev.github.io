d2aa6ee776b179e18a975217e09b7c132d5a15a0
Robin Savage <robin.savage@protonmail.com>
2019-12-28 19:57:27 -0600

    Added JavaScript Files

---
+++ js/Gruntfile.js
@@ -0,0 +1,8 @@
+module.exports = function(grunt) {
+	grunt.initConfig({ 
+		pkg: grunt.file.readJSON('package.json’),
+				// GruntModulesHere
+	)};
+	//grunt.registerTask('default', [‘defaultGruntModule']);
+	//grunt.registerTask(‘doSomething', [‘doSomethingmodule']);
+};
