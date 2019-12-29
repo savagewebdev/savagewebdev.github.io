fd78d42cc9f532126cfbac7f69a6a6f4631b527a
Robin Savage <robin.savage@protonmail.com>
2019-12-28 21:04:44 -0600

    Error quashed

--- js/mapbox.js
+++ js/mapbox.js
@@ -1,5 +1,3 @@
-requirejs.config({
-
 const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iaW51c21heGltdXMiLCJhIjoiY2pzbm5qY2doMGU5dzQ0bjU2eWZiZWJkeCJ9.Cm0M0FhRGX6dnLhQB6wa7g';
