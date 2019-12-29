f065d64421a84f084e4a38d31c407b5a3dd136eb
Robin Savage <robin.savage@protonmail.com>
2019-12-28 19:30:22 -0600

    Added HTML and Boostrap

--- mapbox.js
+++ mapbox.js
@@ -2,6 +2,6 @@ var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iaW51c21heGltdXMiLCJhIjoiY2pzbm5qY2doMGU5dzQ0bjU2eWZiZWJkeCJ9.Cm0M0FhRGX6dnLhQB6wa7g';
 var map = new mapboxgl.Map({
-	container: 'YOUR_CONTAINER_ELEMENT_ID',
+	container: 'map',
 	style: 'mapbox://styles/mapbox/streets-v11'
 });
