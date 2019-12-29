99cc8d34a1a911072da3f7afaa00bf65f9842726
Robin Savage <robin.savage@protonmail.com>
2019-12-28 22:25:58 -0600

    another modular attept

--- js/mapbox.js
+++ js/mapbox.js
@@ -1,4 +1,4 @@
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+import mapboxgl from '../mapbox-gl/dist/mapbox-gl.js';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iaW51c21heGltdXMiLCJhIjoiY2pzbm5qY2doMGU5dzQ0bjU2eWZiZWJkeCJ9.Cm0M0FhRGX6dnLhQB6wa7g';
 const map = new mapboxgl.Map({
