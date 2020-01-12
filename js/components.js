1624e69e7020b7ef1c6a0225db860c5a03069f3b
Robin Savage <robin.savage@protonmail.com>
2020-01-12 15:51:51 -0600

    Require.js added

--- js/components.js
+++ js/components.js
@@ -1,9 +1,6 @@
 ko.components.register('mapbox', {
- 	viewModel: { require: 'js/mapbox' },
+ 	viewModel: { require: 'mapbox.js' },
  	template: { require: '<div class="container-fluid" id="map" style="width: 100%; height: 300px;"></div>' }, 
 });
 
-
-$(document).ready(function(){
 ko.applyBindings();
-}
