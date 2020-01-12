9ab2d64ff6493e3585a3e6eacbefa02aea65a65b
Robin Savage <robin.savage@protonmail.com>
2020-01-12 15:10:58 -0600

    Seeking to refresh

--- js/mapbox.js
+++ js/mapbox.js
@@ -1,4 +1,4 @@
-{
+
 const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iaW51c21heGltdXMiLCJhIjoiY2pzbm5qY2doMGU5dzQ0bjU2eWZiZWJkeCJ9.Cm0M0FhRGX6dnLhQB6wa7g';
@@ -15,4 +15,3 @@ map.addControl(
 		trackUserLocation: true
 	})
 );
-}
